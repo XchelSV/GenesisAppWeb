b34acaef3c2f17f18239da4a804dce66ae2db505
Xchel_Sanchez <xchelsvz@gmail.com>
2017-05-14 13:24:03 -0500

    Static Views

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('show_devotionals');
+  res.render('add_place');
 });
 
 module.exports = router;
