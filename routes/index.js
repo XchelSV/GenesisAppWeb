f556a137be9bcae52a77fc43b2a0892000e64437
Xchel_Sanchez <xchelsvz@gmail.com>
2017-05-15 16:40:28 -0500

    Build Stylus/CSS and Load fonts

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('add_place');
+router.get('/social/list', function(req, res, next) {
+  res.render('index');
+});
+
+router.get('/devotional/list', function(req, res, next) {
+  res.render('show_devotionals');
+});
+
+router.get('/place/list', function(req, res, next) {
+  res.render('show_places');
+});
+
+router.get('/user/list', function(req, res, next) {
+  res.render('show_users');
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login');
 });
 
 module.exports = router;
