8cd88a4eb169ad1edefd8889f5036a2117bd46a2
Xchel_Sanchez <xchelsvz@gmail.com>
2017-05-18 20:42:31 -0500

    Login Functions and Login/Logout Routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+var Client = require('node-rest-client').Client;
+var APIClient = new Client();
+
+
 /* GET home page. */
+router.get('/', function(req,res,next){
+	res.redirect('/social/list');
+});
+
 router.get('/social/list', function(req, res, next) {
   res.render('index');
 });
@@ -19,8 +27,53 @@ router.get('/user/list', function(req, res, next) {
 });
 
 router.get('/login', function(req, res, next) {
-  res.render('login');
+  if(req.session.token){
+  	res.redirect('/');
+  }
+  else{
+  	res.render('login');
+  }
 });
+
+router.get('/logout', function(req, res, next) {
+  APIClient.get("http://localhost:4000/api/logout/"+req.session.token, function (APIData, APIResponse) {
+	   if(APIResponse.statusCode == 401){
+	   		res.sendStatus(404);
+	   }
+	   else{
+	   	if(APIResponse.statusCode == 200){
+		   	req.session.destroy(function(err){
+		   		res.redirect('/');
+		   	})
+		}	
+	   }
+  });
+});
+
+router.post('/authenticate', function(req,res,next){
+
+	var args = {
+	    data: req.body,
+	    headers: { "Content-Type": "application/json" }
+	};
+	
+	APIClient.post("http://localhost:4000/api/login", args, function (APIData, APIResponse) {
+	    
+	    if(APIResponse.statusCode == 404){
+	    	res.sendStatus(401);
+	    }
+	    else{
+	    	if(APIResponse.statusCode == 200){
+	    		//console.log(JSON.parse(APIData.toString()));
+	    		console.log('New Sesion '+APIData.toString());
+	    		req.session.token = APIData.toString();
+	    		res.sendStatus(200);
+	    	}
+	    }
+	    
+	});					
+
+})
 
 router.get('/place/create', function(req, res, next) {
   res.render('add_place');
--- src/js/angularControllers/login_controller.js
+++ src/js/angularControllers/login_controller.js
@@ -1,6 +1,22 @@
 app.controller('loginController', function ($scope, $http, $cookies){
 
-        $scope.cookiesAttempUser = $cookies.attempUser;
-        $scope.cookiesAttempPass = $cookies.attempPass;     
+    $scope.cookiesAttempUser = $cookies.attempUser;
+    $scope.cookiesAttempPass = $cookies.attempPass;
+    $scope.login = function(){
 
-    });
+    	if($scope.email == null || $scope.pass == null){
+    		Materialize.toast('Datos Incompletos', 4000)
+    	}
+    	else{
+	    	var data = {email: $scope.email, pass: $scope.pass};
+			$http.post('/authenticate', data ).then(function success (response){
+				window.location = "/";
+			}, function error (response) {
+				if (response.status === 401) {
+					Materialize.toast('Correo/Contraseña Erróneo', 4000)
+				};
+			});
+	    }
+	}
+
+});
