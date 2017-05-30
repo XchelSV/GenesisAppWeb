aeedbaafe45f6bd51c0a6f40ce07627b7a3f1899
Xchel_Sanchez <xchelsvz@gmail.com>
2017-05-30 16:07:53 -0500

    Delete Place Functions

--- routes/place.js
+++ routes/place.js
@@ -13,7 +13,6 @@ router.get('/place/list', function(req, res, next) {
 });
 
 
-
 router.get('/place/create', function(req, res, next) {
   if(req.session.type === true){
   	res.render('add_place');	
@@ -21,7 +20,24 @@ router.get('/place/create', function(req, res, next) {
   else{
   	res.sendStatus(404);
   }
-  
+});
+
+router.get('/place/delete/:id',function(req,res,next){
+	if(req.session.token){
+		var id = req.params.id;
+		var token = req.session.token
+		APIClient.delete("http://localhost:4000/api/place/"+id+"/"+token, function (APIData, APIResponse) {
+			if (APIResponse.statusCode == 200){
+				res.redirect('/place/list');
+			}
+			else{
+				res.render('error');
+			}
+		});
+	}
+	else{
+		res.render('error');
+	}
 });
 
 module.exports = router;
--- src/js/angularControllers/show_place_controller.js
+++ src/js/angularControllers/show_place_controller.js
@@ -1,44 +1,18 @@
 app.controller('showPlaceController',function  ($scope, $http, $cookies) {
         
-        $scope.session = function(){
-            if($cookies.session != undefined){
-                return true;
-            }
-            else{
-                return false;
-            }
+    $scope.session = function(){
+        if($cookies.session != undefined){
+            return true;
         }
-        $scope.sessionType = function(){
-            if($scope.userType == 'true'){
-                return true;
-            }
-            else{
-                return false;
-            }
+        else{
+            return false;
         }
+    }
 
+    $scope.delete = function(event){
+        $scope.place_id = event.delegateTarget.id;
+        $scope.url = '/place/delete/'+$scope.place_id;
+        $scope.title = 'Eliminar Área';
+    }
         
-        $scope.place_choosen = function(place_id){
-
-            $scope.place_id_selected = place_id;
-            console.log($scope.place_id_selected)
-
-        }
-
-        $scope.delete_place = function(){
-            
-            $http.delete('/place/'+$scope.place_id_selected).
-
-                success(function (data, status, headers, config) {
-                     
-                     window.location.reload();
-
-                     
-                }).
-                error(function (data, status, headers, config) {
-                          alert('Error, Place cant be deleted');
-                });
-
-        }
-
-    });
+});
