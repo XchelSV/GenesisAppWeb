806ac6a08f10a71e8e7f4de6d6d7b5ce489215b9
Xchel_Sanchez <xchelsvz@gmail.com>
2017-10-18 23:25:13 -0500

    Access Constraints from Devotional Routes

--- routes/devotional.js
+++ routes/devotional.js
@@ -7,38 +7,57 @@ var APIClient = new Client();
 
 
 router.get('/devotional/list', function(req, res, next) {
-	APIClient.get("http://localhost:4000/api/devotional", function (APIData, APIResponse) {
-	  res.render('show_devotionals',{devotionals:APIData,type:req.session.type,login:req.session.login});
-	})
+	if(req.session.token == true){
+		APIClient.get("http://localhost:4000/api/devotional", function (APIData, APIResponse) {
+		  res.render('show_devotionals',{devotionals:APIData,type:req.session.type,login:req.session.login});
+		})
+	}
+	else{
+		res.redirect('/');
+	}
 });
 
 router.get('/devotional/read/:_id', function(req, res, next) {
 	var id = req.params._id;
-	APIClient.get("http://localhost:4000/api/devotional/"+id, function (APIData, APIResponse) {
-	  res.render('devotional',{devotional:APIData,type:req.session.type,login:req.session.login});
-	})
+	if(req.session.token == true){
+		APIClient.get("http://localhost:4000/api/devotional/"+id, function (APIData, APIResponse) {
+		  res.render('devotional',{devotional:APIData,type:req.session.type,login:req.session.login});
+		})
+	}
+	else{
+		res.redirect('/');
+	}
 });
 
 router.get('/devotional/edit/:_id', function(req, res, next) {
 	var id = req.params._id;
-	APIClient.get("http://localhost:4000/api/devotional/"+id, function (APIData, APIResponse) {
-	  res.render('edit_devotional',{devotional:APIData._id,type:req.session.type,login:req.session.login});
-	})
+	if(req.session.type == true){
+		APIClient.get("http://localhost:4000/api/devotional/"+id, function (APIData, APIResponse) {
+		  res.render('edit_devotional',{devotional:APIData._id,type:req.session.type,login:req.session.login});
+		})
+	}
+	else{
+		res.redirect('/');
+	}
 });
 
 router.get('/devotional/delete/:_id', function(req, res, next) {
 	var id = req.params._id;
-
-	APIClient.get("http://localhost:4000/api/devotional/"+id, function (APIData, APIResponse2) {
-		APIClient.delete("http://localhost:4000/api/devotional/"+id+"/"+APIData.img+"/"+APIData.audio+"/"+APIData.video+"/"+req.session.token, function (APIData2, APIResponse) {
-			if(APIResponse.statusCode == 200){
-		  		res.redirect('/devotional/list');
-		  	}
-		  	else{
-		  		res.render('error',{login:req.session.login});
-		  	}
+	if(req.session.type == true){
+		APIClient.get("http://localhost:4000/api/devotional/"+id, function (APIData, APIResponse2) {
+			APIClient.delete("http://localhost:4000/api/devotional/"+id+"/"+APIData.img+"/"+APIData.audio+"/"+APIData.video+"/"+req.session.token, function (APIData2, APIResponse) {
+				if(APIResponse.statusCode == 200){
+			  		res.redirect('/devotional/list');
+			  	}
+			  	else{
+			  		res.render('error',{login:req.session.login});
+			  	}
+			})
 		})
-	})
+	}
+	else{
+		res.redirect('/');
+	}
 });
 
 
